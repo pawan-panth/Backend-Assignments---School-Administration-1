f0d4fdeb0ece91cc8a0c1ee28fccc7dffb23ac42
Coder2861 <scriet10@gmail.com>
2020-12-05 14:57:12 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const fs = require("fs");
+let studentArray = require('./InitialData.js');
 const app = express()
 const bodyParser = require("body-parser");
 const port = 8080
@@ -9,8 +11,41 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // your code goes here
+app.get("/api/student", (req, res) => {
+    res.status(201).send(JSON.stringify(studentArray));
+});
 
+app.get("/api/student/:id", (req, res) => {
+    const id = req.params.id;
+    const studentData = studentArray.filter((data) => data.id == id);
+    if (studentData.length > 0) {
+        res.status(201).send(JSON.stringify(studentData));
+    } else {
+        res.sendStatus(404);
+    }
+});
 
+app.post("/api/student",(req,res)=>{
+
+});
+
+app.put("/api/student/:id",(req,res)=>{
+
+});
+
+app.delete("/api/student/:id",(req,res)=>{
+    const id = req.params.id;
+    const studentData = studentArray.filter((data) => data.id == id);
+    if (studentData.length > 0) {
+        const newArray=studentArray.filter((data) => data.id != id);
+        //How to update in InitialData.js file
+        studentArray=newArray;
+        console.log(studentArray);
+        res.status(201).send(JSON.stringify(newArray));
+    } else {
+        res.sendStatus(404);
+    }
+});
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
 module.exports = app;   
